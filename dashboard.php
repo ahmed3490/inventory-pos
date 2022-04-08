3f5f16a2d9bcecbd2660337bd2e6103f2bf475f9
Hassan Muaz <muax000@gmail.com>
2022-04-09 01:57:15 +0500

    first commit

---
+++ dashboard.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html>
+<head>
+<?php include('includes/dashboard/header.php'); ?> 
+</head>
+
+<body class="hold-transition skin-blue sidebar-mini">
+<?php include('includes/dashboard/body.php'); ?> 
+  
+<?php include('includes/dashboard/footer.php'); ?> 
+
+<?php include('includes/dashboard/js-scripts.php'); ?> 
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include('includes/login/login-header.php'); ?> 
+</head>
+
+<body class="hold-transition login-page">
+     <?php include('includes/login/login-body.php'); ?> 
+     <?php include('includes/login/login-script.php'); ?>     
+</script>
+</body>
+</html>
